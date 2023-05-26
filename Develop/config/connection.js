348a171ae4c158a493116bfb670831875090b9bb
Luis Martinez <luismtz@Luiss-Air.lan>
2023-05-26 11:56:56 -0500

    fished connection issues

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,15 +1,45 @@
-require('dotenv').config();
+// require('dotenv').config();
+
+// const Sequelize = require('sequelize');
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     // : new sequelize(DB_NAME= 'ecommerce_db', DB_USER= 'root', DB_PASSWORD= '605291Lmtz$',{   
+//       host: '127.0. 0.1.',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+
+//       port: 3001,
+//     });
+
+// module.exports = sequelize;
 
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    // process.env.DB_NAME,
+    // process.env.DB_USER,
+    // process.env.DB_PASSWORD,
+    DB_NAME='ecommerce_db',
+    DB_USER='root',
+    DB_PW='605291Lmtz$',
+    {
+      host: '127.0.0.1.',
       dialect: 'mysql',
-      dialectOptions: {
+      dialectOption:{
         decimalNumbers: true,
       },
-    });
+    }
+  );
+}
 
 module.exports = sequelize;
